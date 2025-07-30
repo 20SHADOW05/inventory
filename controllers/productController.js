16b717fbb6df9dafbdf2a02b895f20902ee4d1c8
Ankababu Yadav <146865769+20SHADOW05@users.noreply.github.com>
2025-07-30 19:00:31 +0530

    products route

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,7 @@ const pool = require('../config/db');
 async function load_products(req, res){
     const categories = await pool.query('SELECT DISTINCT category FROM default_products');
     const products = await pool.query('SELECT * FROM default_products');
-    res.render('products' , { categories: categories.rows });
+    res.render('products' , { categories: categories.rows , products: products.rows });
 }
 
 module.exports = { load_products }
--- seed.js
+++ seed.js
@@ -4,39 +4,39 @@ async function seed() {
   const products = [
 
     // Action
-    ['Apex Legends', 499, '/images/action/apex_legends.png', 'action'],
-    ['Assasins Creed', 599, '/images/action/assasins_creed.png', 'action'],
-    ['Cs2', 699, '/images/action/cs2.png', 'action'],
-    ['Red Dead Redemption', 549, '/images/action/red_dead_redemption.png', 'action'],
-    ['Sekiro', 649, '/images/action/sekiro.png', 'action'],
+    ['Apex Legends', 499, '/images/action/apex_legends.jpg', 'action'],
+    ['Assasins Creed', 599, '/images/action/assasins_creed.jpg', 'action'],
+    ['Cs2', 699, '/images/action/cs2.jpg', 'action'],
+    ['Red Dead Redemption', 549, '/images/action/red_dead_redemption.jpg', 'action'],
+    ['Sekiro', 649, '/images/action/sekiro.jpg', 'action'],
 
     // Adventure
-    ['Borderlands4', 449, '/images/adventure/borderlands4.png', 'adventure'],
-    ['Forza Horizon', 529, '/images/adventure/forza_horizon.png', 'adventure'],
-    ['Outer Wilds', 579, '/images/adventure/outer_wilds.png', 'adventure'],
-    ['Peak', 499, '/images/adventure/peak.png', 'adventure'],
-    ['Terraria', 619, '/images/adventure/terraria.png', 'adventure'],
+    ['Borderlands4', 449, '/images/adventure/borderlands4.jpg', 'adventure'],
+    ['Forza Horizon', 529, '/images/adventure/forza_horizon.jpg', 'adventure'],
+    ['Outer Wilds', 579, '/images/adventure/outer_wilds.jpg', 'adventure'],
+    ['Peak', 499, '/images/adventure/peak.jpg', 'adventure'],
+    ['Terraria', 619, '/images/adventure/terraria.jpg', 'adventure'],
 
     // Building & Automation
-    ['Fabledom', 799, '/images/building_automation/fabledom.png', 'building-automation'],
-    ['Once Human', 899, '/images/building_automation/once_human.png', 'building-automation'],
-    ['Rimworld', 849, '/images/building_automation/rimworld.png', 'building-automation'],
-    ['Rust', 769, '/images/building_automation/rust.png', 'building-automation'],
-    ['Stardew Valley', 929, '/images/building_automation/stardew_valley.png', 'building-automation'],
+    ['Fabledom', 799, '/images/building_automation/fabledom.jpg', 'building-automation'],
+    ['Once Human', 899, '/images/building_automation/once_human.jpg', 'building-automation'],
+    ['Rimworld', 849, '/images/building_automation/rimworld.jpg', 'building-automation'],
+    ['Rust', 769, '/images/building_automation/rust.jpg', 'building-automation'],
+    ['Stardew Valley', 929, '/images/building_automation/stardew_valley.jpg', 'building-automation'],
 
     // Sports
-    ['Bmx Streets', 499, '/images/sports/bmx_streets.png', 'sports'],
-    ['Fc25', 559, '/images/sports/fc25.png', 'sports'],
-    ['Fishing Planet', 649, '/images/sports/fishing_planet.png', 'sports'],
-    ['Rugby League', 729, '/images/sports/rugby_league.png', 'sports'],
-    ['Undisputed', 699, '/images/sports/undisputed.png', 'sports'],
+    ['Bmx Streets', 499, '/images/sports/bmx_streets.jpg', 'sports'],
+    ['Fc25', 559, '/images/sports/fc25.jpg', 'sports'],
+    ['Fishing Planet', 649, '/images/sports/fishing_planet.jpg', 'sports'],
+    ['Rugby League', 729, '/images/sports/rugby_league.jpg', 'sports'],
+    ['Undisputed', 699, '/images/sports/undisputed.jpg', 'sports'],
 
     // Strategy
-    ['9kings', 599, '/images/strategy/9kings.png', 'strategy'],
-    ['Blood Bar Tycoon', 689, '/images/strategy/blood_bar_tycoon.png', 'strategy'],
-    ['Limbus Company', 739, '/images/strategy/limbus_company.png', 'strategy'],
-    ['Persona 3 Reload', 819, '/images/strategy/persona_3_reload.png', 'strategy'],
-    ['The King Is Watching', 559, '/images/strategy/the_king_is_watching.png', 'strategy']
+    ['9kings', 599, '/images/strategy/9kings.jpg', 'strategy'],
+    ['Blood Bar Tycoon', 689, '/images/strategy/blood_bar_tycoon.jpg', 'strategy'],
+    ['Limbus Company', 739, '/images/strategy/limbus_company.jpg', 'strategy'],
+    ['Persona 3 Reload', 819, '/images/strategy/persona_3_reload.jpg', 'strategy'],
+    ['The King Is Watching', 559, '/images/strategy/the_king_is_watching.jpg', 'strategy']
   ];
 
   for (const [name, price, path, category] of products) {
