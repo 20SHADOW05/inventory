a3789feb7fe8bfd5170eeddc75242225a88c953f
Ankababu Yadav <146865769+20SHADOW05@users.noreply.github.com>
2025-08-04 11:25:24 +0530

    quering the db according to user logIn status

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,26 +2,50 @@ const { use } = require('passport');
 const pool = require('../config/db');
 
 async function load_products(req, res){
+    let userExists = req.user ? req.user.id : false;
+
     const selected_categories = [].concat(req.query.categories || []);
 
-    let query = 'SELECT * FROM default_products';
-    let params = [];
+    let products,categories;
+    if(userExists){
+        let query = `SELECT up.user_id, up.product_id, up.product_name, uc.category, up.price, up.image_path
+                    FROM user_products up
+                    INNER JOIN user_categories uc
+                    ON up.category_id = uc.category_id
+                    WHERE up.user_id = $1`;
+        let params = [userExists];
 
-    if(selected_categories.length > 0){
-        query += ' WHERE category = ANY($1)';
-        params = [selected_categories];
+        if (selected_categories.length > 0) {
+            const cat_ids_result = await pool.query(
+                'SELECT category_id FROM user_categories WHERE user_id = $1 AND category = ANY($2)',
+                [userExists, selected_categories]
+            );
+
+            const selected_categories_id = cat_ids_result.rows.map(row => row.category_id);
+
+            query += ' AND up.category_id = ANY($2)';
+            params.push(selected_categories_id);    
+        }      
+
+        categories = await pool.query('SELECT category FROM user_categories WHERE user_id=$1',[userExists])
+        products = await pool.query(query, params);
+
     }
 
-    const products = params.length > 0 ? await pool.query(query, params) : await pool.query('SELECT * FROM default_products');
+    else{
+        let query = 'SELECT * FROM default_products';
+        let params = [];
 
-    const categories = await pool.query('SELECT DISTINCT category FROM default_products');
+        if(selected_categories.length > 0){
+            query += ' WHERE category = ANY($1)';
+            params = [selected_categories];
+        }
 
-    let userIn = false;
-    if(req.user){
-        userIn = true;
+        categories = await pool.query('SELECT DISTINCT category FROM default_products');
+        products = selected_categories.length > 0 ? await pool.query(query, params) : await pool.query('SELECT * FROM default_products');
     }
-    
-    res.render('products' , { categories: categories.rows , products: products.rows , selected_categories , userIn});
+
+    res.render('products' , { categories: categories.rows , products: products.rows , selected_categories , userIn: userExists});
 }
 
 async function search_products(req, res){
